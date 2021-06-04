dd12b137cceb97167bad004baf2975188049526b
MKALYAN2002 <65495462+MKALYAN2002@users.noreply.github.com>
2021-06-04 11:24:39 +0530

    Updated Monte Carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -43,24 +43,29 @@ float mc_pi(int n){
   int i=0,circle=0;
   float z;
   int square=0;
+  
   float pi;
+  
   while (i<n-1){
+   
    x=frandom();
    y=frandom();
    z=x*x+y*y;
-   if(z<=1)
-   {
+  
+   
+   if(z<=1){
      circle+=1;
      square+=1;
      }
-   else
-   {
+   else{
      square+=1;
-     i++; 
-   }  
+   
+   i++;
+     
    }
+   
+   }
+ 
  pi=(float)4*circle/square;
- return pi;  
- }
-
-
+ return pi; 
+}
